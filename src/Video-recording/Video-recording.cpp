10bbc2488c26848604a293276b2a9f3a47825866
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-10-30 21:17:02 +0300

    start

---
+++ src/Video-recording/Video-recording.cpp
@@ -0,0 +1,73 @@
+﻿#include <Windows.h>
+#include "Video-recording.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        return 0;
+    }
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    default:
+    {
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hPrevInstance, HINSTANCE hInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    static TCHAR className[] = TEXT("GameClass");
+    static TCHAR windowName[] = TEXT("WinApi");
+
+    WNDCLASSEX wcex;
+
+    wcex.cbClsExtra = 0;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.cbWndExtra = 0;
+    wcex.hbrBackground = NULL;
+    wcex.hCursor = LoadCursor(hInstance, IDC_ARROW);
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hIconSm = NULL;
+    wcex.hInstance = hInstance;
+    wcex.lpfnWndProc = WndProc;
+    wcex.lpszClassName = className;
+    wcex.lpszMenuName = NULL;
+    wcex.style = 0;
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    HWND hWnd = CreateWindow(className, windowName, WS_OVERLAPPEDWINDOW, 0, 0, 800, 800, NULL, NULL, hInstance, NULL);
+    if (!hWnd)
+        return 0;
+
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (true)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                break;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+
+    }
+
+    return msg.wParam;
+}
