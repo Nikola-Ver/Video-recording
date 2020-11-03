982b6e29fa42c9a8860d3b01d8fcb8e65087ef83
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-11-03 19:31:47 +0300

    added gif converter, conected Magick++

--- src/Video-recording/Video-recording.cpp
+++ src/Video-recording/Video-recording.cpp
@@ -1,17 +1,159 @@
-﻿#include <Windows.h>
+﻿// Video-recording.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
 #include "Video-recording.h"
+#include <Magick++.h>
+
+#define N 1
+#define TIMER_ID 1
+
+RECT rcSize;
+HDC hdcBackBuffer, hdcTable;
+PAINTSTRUCT  ps;
+
+std::vector<Magick::Image> frames;
+int delay = 100;
+int maxFrames = 10;
+std::string resolution = "1920";
+bool flagRecording = true;
+
+BOOL SaveHBITMAPToFile(HBITMAP hBitmap, LPCTSTR lpszFileName)
+{
+    HDC hDC;
+    int iBits;
+    WORD wBitCount;
+    DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
+    BITMAP Bitmap0;
+    BITMAPFILEHEADER bmfHdr;
+    BITMAPINFOHEADER bi;
+    LPBITMAPINFOHEADER lpbi;
+    HANDLE fh, hDib, hPal, hOldPal2 = NULL;
+    hDC = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
+    iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES);
+    DeleteDC(hDC);
+    if (iBits <= 1)
+        wBitCount = 1;
+    else if (iBits <= 4)
+        wBitCount = 4;
+    else if (iBits <= 8)
+        wBitCount = 8;
+    else
+        wBitCount = 24;
+    GetObject(hBitmap, sizeof(Bitmap0), (LPSTR)&Bitmap0);
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = Bitmap0.bmWidth;
+    bi.biHeight = -Bitmap0.bmHeight;
+    bi.biPlanes = 1;
+    bi.biBitCount = wBitCount;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrImportant = 0;
+    bi.biClrUsed = 256;
+    dwBmBitsSize = ((Bitmap0.bmWidth * wBitCount + 31) & ~31) / 8
+        * Bitmap0.bmHeight;
+    hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
+    lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
+    *lpbi = bi;
+
+    hPal = GetStockObject(DEFAULT_PALETTE);
+    if (hPal)
+    {
+        hDC = GetDC(NULL);
+        hOldPal2 = SelectPalette(hDC, (HPALETTE)hPal, FALSE);
+        RealizePalette(hDC);
+    }
+
+
+    GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap0.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
+        + dwPaletteSize, (BITMAPINFO*)lpbi, DIB_RGB_COLORS);
+
+    if (hOldPal2)
+    {
+        SelectPalette(hDC, (HPALETTE)hOldPal2, TRUE);
+        RealizePalette(hDC);
+        ReleaseDC(NULL, hDC);
+    }
+
+    fh = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+
+    if (fh == INVALID_HANDLE_VALUE)
+        return FALSE;
+
+    bmfHdr.bfType = 0x4D42; // "BM"
+    dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
+    bmfHdr.bfSize = dwDIBSize;
+    bmfHdr.bfReserved1 = 0;
+    bmfHdr.bfReserved2 = 0;
+    bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
+
+    WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+
+    WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL);
+    GlobalUnlock(hDib);
+    GlobalFree(hDib);
+    CloseHandle(fh);
+    return TRUE;
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    HDC          hdc;
+
     switch (msg)
     {
+    case WM_TIMER:
+    {
+        InvalidateRect(hWnd, &rcSize, true);
+        return 0;
+    }
+    case WM_SIZE:
+    {
+        GetClientRect(hWnd, &rcSize);
+        return 0;
+    }
     case WM_CREATE:
     {
+        SetTimer(hWnd, TIMER_ID, delay, NULL);
         return 0;
     }
     case WM_DESTROY:
     {
+        KillTimer(hWnd, TIMER_ID);
         PostQuitMessage(0);
+        return 0;
+    }
+    case WM_PAINT:
+    {
+        BeginPaint(hWnd, &ps);
+        
+        // Record logic
+        if (frames.size() > 0 || flagRecording)
+        {
+            using namespace Magick;
+            if (frames.size() < maxFrames && flagRecording)
+            {
+                Image img("screenshot:");
+                img.crop(Geometry(1000,1000,100,100));
+                //img.trim();
+                //img.chop(Geometry("700x700"));
+                img.animationDelay(delay / 10);
+                //img.resize(Geometry(resolution));
+                frames.push_back(img);
+            }
+            else
+            {
+                writeImages(frames.begin(), frames.end(), "D:/das.gif");
+                frames.clear();
+                frames.clear();
+            }
+        }
+
+        EndPaint(hWnd, &ps);
+
         return 0;
     }
     default:
@@ -52,6 +194,7 @@ int WINAPI WinMain(HINSTANCE hPrevInstance, HINSTANCE hInstance, LPSTR lpCmdLine
     UpdateWindow(hWnd);
 
     MSG msg;
+
     while (true)
     {
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -66,7 +209,6 @@ int WINAPI WinMain(HINSTANCE hPrevInstance, HINSTANCE hInstance, LPSTR lpCmdLine
                 DispatchMessage(&msg);
             }
         }
-
     }
 
     return msg.wParam;
