99ab763a41b4902f33f0126ce6c0de166cdfe59b
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-11-04 02:49:08 +0300

    added croping

--- src/Video-recording/Video-recording.cpp
+++ src/Video-recording/Video-recording.cpp
@@ -17,86 +17,12 @@ int delay = 100;
 int maxFrames = 10;
 std::string resolution = "1920";
 bool flagRecording = true;
+HHOOK _hook;
 
-BOOL SaveHBITMAPToFile(HBITMAP hBitmap, LPCTSTR lpszFileName)
+LRESULT CALLBACK KeyDownProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    HDC hDC;
-    int iBits;
-    WORD wBitCount;
-    DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
-    BITMAP Bitmap0;
-    BITMAPFILEHEADER bmfHdr;
-    BITMAPINFOHEADER bi;
-    LPBITMAPINFOHEADER lpbi;
-    HANDLE fh, hDib, hPal, hOldPal2 = NULL;
-    hDC = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
-    iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES);
-    DeleteDC(hDC);
-    if (iBits <= 1)
-        wBitCount = 1;
-    else if (iBits <= 4)
-        wBitCount = 4;
-    else if (iBits <= 8)
-        wBitCount = 8;
-    else
-        wBitCount = 24;
-    GetObject(hBitmap, sizeof(Bitmap0), (LPSTR)&Bitmap0);
-    bi.biSize = sizeof(BITMAPINFOHEADER);
-    bi.biWidth = Bitmap0.bmWidth;
-    bi.biHeight = -Bitmap0.bmHeight;
-    bi.biPlanes = 1;
-    bi.biBitCount = wBitCount;
-    bi.biCompression = BI_RGB;
-    bi.biSizeImage = 0;
-    bi.biXPelsPerMeter = 0;
-    bi.biYPelsPerMeter = 0;
-    bi.biClrImportant = 0;
-    bi.biClrUsed = 256;
-    dwBmBitsSize = ((Bitmap0.bmWidth * wBitCount + 31) & ~31) / 8
-        * Bitmap0.bmHeight;
-    hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
-    lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
-    *lpbi = bi;
-
-    hPal = GetStockObject(DEFAULT_PALETTE);
-    if (hPal)
-    {
-        hDC = GetDC(NULL);
-        hOldPal2 = SelectPalette(hDC, (HPALETTE)hPal, FALSE);
-        RealizePalette(hDC);
-    }
-
-
-    GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap0.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
-        + dwPaletteSize, (BITMAPINFO*)lpbi, DIB_RGB_COLORS);
-
-    if (hOldPal2)
-    {
-        SelectPalette(hDC, (HPALETTE)hOldPal2, TRUE);
-        RealizePalette(hDC);
-        ReleaseDC(NULL, hDC);
-    }
-
-    fh = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
-
-    if (fh == INVALID_HANDLE_VALUE)
-        return FALSE;
-
-    bmfHdr.bfType = 0x4D42; // "BM"
-    dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
-    bmfHdr.bfSize = dwDIBSize;
-    bmfHdr.bfReserved1 = 0;
-    bmfHdr.bfReserved2 = 0;
-    bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
-
-    WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
-
-    WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL);
-    GlobalUnlock(hDib);
-    GlobalFree(hDib);
-    CloseHandle(fh);
-    return TRUE;
+    MessageBox(NULL, L"F1 is pressed!", L"key pressed", MB_ICONINFORMATION);
+    return CallNextHookEx(_hook, nCode, wParam, lParam);
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -120,7 +46,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         SetTimer(hWnd, TIMER_ID, delay, NULL);
         return 0;
     }
-    case WM_DESTROY:
+    case WM_KEYDOWN:
     {
         KillTimer(hWnd, TIMER_ID);
         PostQuitMessage(0);
@@ -129,7 +55,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
     {
         BeginPaint(hWnd, &ps);
-        
+
         // Record logic
         if (frames.size() > 0 || flagRecording)
         {
@@ -137,7 +63,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             if (frames.size() < maxFrames && flagRecording)
             {
                 Image img("screenshot:");
-                img.crop(Geometry(1000,1000,100,100));
+                img.crop(Geometry(500, 500, 200, 200));
+                //img.crop(Geometry("500x500+200+200"));
+                img.repage();
                 //img.trim();
                 //img.chop(Geometry("700x700"));
                 img.animationDelay(delay / 10);
@@ -155,10 +83,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         EndPaint(hWnd, &ps);
 
         return 0;
-    }
-    default:
-    {
-        return DefWindowProc(hWnd, msg, wParam, lParam);
     }
     }
 }
@@ -186,12 +110,14 @@ int WINAPI WinMain(HINSTANCE hPrevInstance, HINSTANCE hInstance, LPSTR lpCmdLine
     if (!RegisterClassEx(&wcex))
         return 0;
 
-    HWND hWnd = CreateWindow(className, windowName, WS_OVERLAPPEDWINDOW, 0, 0, 800, 800, NULL, NULL, hInstance, NULL);
+    HWND hWnd = CreateWindowEx(WS_EX_TOOLWINDOW, className, windowName, NULL, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
     if (!hWnd)
         return 0;
 
     ShowWindow(hWnd, nShowCmd);
     UpdateWindow(hWnd);
+
+    SetWindowsHookEx(WH_KEYBOARD, KeyDownProc, NULL, 0);
 
     MSG msg;
 
